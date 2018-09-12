c8d40a5cdc28dfbf3a9d31de2db6338fd21b74be
brice <brice@hotmail.com>
2018-09-12 09:22:31 +0200

    first commit

---
+++ src/test/java/seleniumTest.java
@@ -0,0 +1,52 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class seleniumTest {
+
+    WebDriver driver ;
+    @Before
+    public void setup()
+    {
+        driver = new FirefoxDriver();
+        driver.get("https://www.google.fr");
+    }
+
+    @After
+    public void quitBrowser()
+    {
+        driver.quit();
+    }
+
+
+
+    @Test
+    public void test1() throws InterruptedException {
+
+        WebElement barrerecherche = driver.findElement(By.id("lst-ib"));
+        barrerecherche.sendKeys("canelé");
+        barrerecherche.sendKeys(Keys.ENTER);
+        Thread.sleep(1500);
+
+    }
+
+   @Test
+   public void test3() throws InterruptedException {
+
+
+       WebElement barrerecherche = driver.findElement(By.id("lst-ib"));
+       barrerecherche.sendKeys("manelé");
+       Thread.sleep(1500);
+       WebElement buttonrecherche = driver.findElement(By.className("lsb"));
+       buttonrecherche.click();
+
+
+}
+}
+
